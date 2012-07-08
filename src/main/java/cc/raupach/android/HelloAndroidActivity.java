a22ee3434906709b59bc68e60c6d56656c237f15
raupach <oliver@raupach.cc>
2012-07-08 19:30:10 +0200

    .

---
+++ src/main/java/cc/raupach/android/HelloAndroidActivity.java
@@ -0,0 +1,30 @@
+package cc.raupach.android;
+
+import org.apache.commons.math.util.BigReal;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class HelloAndroidActivity extends Activity
+{
+
+   private static String TAG = "my-android-application";
+
+   /**
+    * Called when the activity is first created.
+    * 
+    * @param savedInstanceState
+    *           If the activity is being re-initialized after previously being shut down then this Bundle contains the data it most recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is null.</b>
+    */
+   @Override
+   public void onCreate(Bundle savedInstanceState)
+   {
+      super.onCreate(savedInstanceState);
+      
+      BigReal sd = BigReal.ONE;
+      Log.i(TAG, "onCreate "+sd.toString());
+      setContentView(R.layout.main);
+   }
+
+}
